30fdfd5bd9c54828115298d4b0b948b749d65442
MatthiasChometon <matthiaschometon787@gmail.com>
2022-10-19 00:47:46 +0200

    feat: add typeorm and gql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,47 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
+import { Module } from '@nestjs/common'
+import { GraphQLModule } from '@nestjs/graphql'
+import { join } from 'path'
+import { UserModule } from './user/user.module'
+import { AuthModule } from './auth/auth.module'
+import { ConfigModule } from '@nestjs/config/dist/config.module'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { EmailModule } from './email/email.module'
+import { GraphQLError, GraphQLFormattedError } from 'graphql'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+      introspection: true,
+      playground: true,
+      cache: 'bounded',
+      persistedQueries: {
+        ttl: 900,
+      },
+      formatError: (error: GraphQLError) => {
+        const graphQLFormattedError: GraphQLFormattedError = {
+          message: error?.message,
+        }
+        return graphQLFormattedError
+      },
+    }),
+    TypeOrmModule.forRoot({
+      type: process.env.DATABASE_TYPE as any,
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      autoLoadEntities: true,
+      synchronize: process.env.DATABASE_SYNCHRONIZE === 'true',
+    }),
+    EmailModule,
+    UserModule,
+    AuthModule,
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { NestExpressApplication } from '@nestjs/platform-express'
+import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
+  })
+  app.useGlobalPipes(new ValidationPipe())
+  app.enableCors({
+    origin: process.env.CORS_ORIGIN,
+  })
+  const port = process.env.PORT || 4000
+  await app.listen(port)
+  console.log(`server listen on localhost:${port}/graphql`)
 }
-bootstrap();
+bootstrap()
