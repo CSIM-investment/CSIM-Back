e87ce34b5c55208228eaab24b5f36ddc16a6cccf
MatthiasChometon <matthiaschometon787@gmail.com>
2022-11-15 19:55:31 +0100

    feat: add tokens objects

---
+++ src/auth/models/refresh-token.model.ts
@@ -0,0 +1,36 @@
+import { UnauthorizedException } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
+import { User } from 'src/user/methods/user.methods'
+import { Repository } from 'typeorm'
+import { Token } from './token.model'
+
+export class RefreshToken extends Token {
+  constructor(
+    jwtService: JwtService,
+    private userRepository: Repository<User>,
+  ) {
+    super(
+      process.env.REFRESH_TOKEN_EXPIRATION_TIME,
+      jwtService,
+      process.env.REFRESH_TOKEN_SECRET,
+    )
+  }
+
+  private async verify(refreshToken: string): Promise<void> {
+    try {
+      await this.jwtService.verifyAsync(refreshToken, {
+        secret: this.secret,
+      })
+    } catch (err) {
+      throw new UnauthorizedException('Refresh token not valid')
+    }
+  }
+
+  public async resolve(id: number, refreshToken: string): Promise<User> {
+    const [user] = await Promise.all([
+      this.userRepository.findOneByOrFail({ refreshToken, id }),
+      this.verify(refreshToken),
+    ])
+    return user
+  }
+}
