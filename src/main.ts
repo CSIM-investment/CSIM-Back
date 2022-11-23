4be3334e887236668b0af1278719d3d852d89952
Tim GIMENEZ <tim.gimenez26@gmail.com>
2022-11-23 17:02:35 +0100

    feat: YahooFinanceService

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { NestExpressApplication } from '@nestjs/platform-express'
 import { ValidationPipe } from '@nestjs/common'
+import {YahooFinanceService} from "./yahooFinance/yahooFinance.service";
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
---
+++ src/yahooFinance/yahooFinance.service.ts
@@ -0,0 +1,55 @@
+import {Injectable} from "@nestjs/common";
+import * as moment from 'moment';
+const yahooFinance = require('yahoo-finance');
+
+@Injectable()
+export class YahooFinanceService{
+    private readonly yahooFinanceCurrencyName: string;
+
+    constructor(
+        private stockOrCryptoName: string,
+        private currency: string = "EUR"
+    ) {
+        this.yahooFinanceCurrencyName = stockOrCryptoName + '-' + currency;
+    }
+
+    async checkCurrency(){
+        if (!['USD', 'EUR'].includes(this.currency)) {
+            throw Error("")
+        }
+    }
+
+    async getHistory(beginDate?: moment.Moment, endDate?: moment.Moment){
+        if(beginDate === undefined && endDate === undefined){
+            endDate = moment();
+        }
+
+        if(beginDate === undefined){
+            beginDate = endDate
+            beginDate = endDate.subtract(1, 'day')
+        }
+
+        if(endDate === undefined){
+            beginDate = endDate.add(1, 'day')
+        }
+
+        let data = [];
+
+        let beginDateString = moment(beginDate).format("YYYY-MM-DD");
+        let endDateString = moment(beginDate).format("YYYY-MM-DD");
+
+        await yahooFinance.historical({
+            symbol: this.yahooFinanceCurrencyName,
+            from: beginDateString,
+            to: endDateString,
+        }, async function(err, quotes){
+            if(err){
+                throw Error(err);
+            }
+
+            data = quotes
+        })
+
+        return data;
+    }
+}
