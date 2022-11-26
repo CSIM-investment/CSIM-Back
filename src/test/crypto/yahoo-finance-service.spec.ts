defd529e4509b386c0b888d14d82663b8a2e254d
Tim GIMENEZ <tim.gimenez26@gmail.com>
2022-11-26 23:21:15 +0100

    refacto: tests of getHistoricalOfBTC with yahoo finance service

--- src/test/crypto/yahoo-finance-service.spec.ts
+++ src/test/crypto/yahoo-finance-service.spec.ts
@@ -1,20 +1,37 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { YahooFinanceService } from '../../crypto/yahoo-finance.service'
+import * as dayjs from 'dayjs'
 
 describe('YahooFinanceServiceController', () => {
   let service: YahooFinanceService
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [
-        YahooFinanceService,
-      ],
-    }).compile()
-    service = await module.get(YahooFinanceService)
+  it('getHistoricalOfBTCWithoutParameterInFunctionWork', async () => {
+    const data = await new YahooFinanceService('BTC').getHistory()
+    expect(data.length > 0).toBeTruthy()
   })
 
-  it('getHistoricalOfBTCWithoutParametterInFunctionWork', async () => {
-    const data = await new YahooFinanceService('BTC').getHistory()
-    console.log(data)
+  it('getHistoricalOfBTCWithOnlyBeginDateParameterInFunctionWork', async () => {
+    const beginDate = dayjs('2022-02-22', 'YYYY-MM-JJ')
+    const data = await new YahooFinanceService('BTC').getHistory(beginDate)
+    expect(data.length > 0).toBeTruthy()
+  })
+
+  it('getHistoricalOfBTCWithOnlyEndDateParameterInFunctionWork', async () => {
+    const beginDate = dayjs('2022-02-22', 'YYYY-MM-JJ')
+    const data = await new YahooFinanceService('BTC').getHistory(
+      undefined,
+      dayjs('2022-02-22', 'YYYY-MM-JJ'),
+    )
+    expect(data.length > 0).toBeTruthy()
+  })
+
+  it('getHistoricalOfBTCWithAllParameterInFunctionWork', async () => {
+    const beginDate = dayjs('2022-02-22', 'YYYY-MM-JJ')
+    const endDate = dayjs('2022-11-26', 'YYYY-MM-JJ')
+    const data = await new YahooFinanceService('BTC').getHistory(
+      beginDate,
+      endDate,
+    )
+    expect(data.length > 0).toBeTruthy()
   })
 })
