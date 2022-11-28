3befaef80dad29776d9e75c5b6aab2e4cf99b0b8
MatthiasChometon <matthiaschometon787@gmail.com>
2022-11-28 21:38:50 +0100

    refactor: add buildFromAndToForHistory function

--- src/crypto/yahoo-finance.service.ts
+++ src/crypto/yahoo-finance.service.ts
@@ -18,16 +18,22 @@ export class YahooFinanceService {
       throw Error('Currency must be USD or EUR !')
   }
 
-  async getHistory(beginDate?: dayjs.Dayjs, endDate?: dayjs.Dayjs) {
+  private buildFromAndToForHistory(
+    beginDate?: Date,
+    endDate?: Date,
+  ): { from: string; to: string } {
+    const beginDateDayjs = dayjs(endDate ?? new Date())
+    const endDateDayjs = dayjs(beginDate) ?? beginDateDayjs.subtract(1, 'day')
+
+    const from = dayjs(beginDateDayjs).format('YYYY-MM-DD')
+    const to = dayjs(endDateDayjs).format('YYYY-MM-DD')
+
+    return { from, to }
+  }
+
+  async getHistory(beginDate?: Date, endDate?: Date) {
     await this.checkCurrency()
-    endDate = beginDate && endDate ? endDate : dayjs()
-
-    beginDate = beginDate ? beginDate : endDate.subtract(1, 'day')
-
-    endDate = endDate ? endDate : beginDate.add(1, 'day')
-
-    const from = dayjs(beginDate).format('YYYY-MM-DD')
-    const to = dayjs(endDate).format('YYYY-MM-DD')
+    const { from, to } = this.buildFromAndToForHistory(beginDate, endDate)
 
     return await yahooFinance.historical(
       {
@@ -35,9 +41,8 @@ export class YahooFinanceService {
         from,
         to,
       },
-      async function (err, quotes) {
-        if (err) throw Error(err)
-
+      async function (err: string, quotes) {
+        if (err) throw new Error(err)
         return quotes
       },
     )
