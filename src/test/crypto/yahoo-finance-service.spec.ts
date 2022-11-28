5c7bc25aaad84934db5894efb727e64c1e3b421c
MatthiasChometon <matthiaschometon787@gmail.com>
2022-11-28 21:45:50 +0100

    test: add beginDate and EndDate to beforeEach and remove dayjs

--- src/test/crypto/yahoo-finance-service.spec.ts
+++ src/test/crypto/yahoo-finance-service.spec.ts
@@ -1,9 +1,13 @@
-import { Test, TestingModule } from '@nestjs/testing'
 import { YahooFinanceService } from '../../crypto/yahoo-finance.service'
-import * as dayjs from 'dayjs'
 
 describe('Yahoo Finance Service Controller', () => {
-  let service: YahooFinanceService
+  let beginDate: Date
+  let endDate: Date
+
+  beforeEach(() => {
+    beginDate = new Date()
+    endDate = new Date('2022-11-26')
+  })
 
   it('get historical of BTC without parameter in function work', async () => {
     const data = await new YahooFinanceService('BTC').getHistory()
@@ -11,7 +15,6 @@ describe('Yahoo Finance Service Controller', () => {
   })
 
   it('get historical of BTC with only begin date parameter in function work', async () => {
-    const beginDate = dayjs('2022-02-22', 'YYYY-MM-JJ')
     const data = await new YahooFinanceService('BTC').getHistory(beginDate)
     expect(data.length > 0).toBeTruthy()
   })
@@ -19,14 +22,12 @@ describe('Yahoo Finance Service Controller', () => {
   it('get historical of BTC with only end date parameter in function work', async () => {
     const data = await new YahooFinanceService('BTC').getHistory(
       undefined,
-      dayjs('2022-02-22', 'YYYY-MM-JJ'),
+      endDate,
     )
     expect(data.length > 0).toBeTruthy()
   })
 
   it('get historical of BTC with all parameter in function work', async () => {
-    const beginDate = dayjs('2022-02-22', 'YYYY-MM-JJ')
-    const endDate = dayjs('2022-11-26', 'YYYY-MM-JJ')
     const data = await new YahooFinanceService('BTC').getHistory(
       beginDate,
       endDate,
