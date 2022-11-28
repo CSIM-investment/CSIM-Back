7951150c7586787eabe495aeed1f49994d6a1411
Tim GIMENEZ <tim.gimenez26@gmail.com>
2022-11-28 20:41:59 +0100

    refacto: remove deadcode

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { NestExpressApplication } from '@nestjs/platform-express'
 import { ValidationPipe } from '@nestjs/common'
-import {YahooFinanceService} from "./crypto/yahoo-finance.service";
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
@@ -14,8 +13,6 @@ async function bootstrap() {
   })
   const port = process.env.PORT || 4000
 
-  console.log(await new YahooFinanceService('SHIB').getHistory())
-  return;
   await app.listen(port)
   console.log(`server listen on localhost:${port}/graphql`)
 }
