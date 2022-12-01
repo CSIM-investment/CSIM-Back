0589cc4bc27ba43c3550556b46cefaf544644a1e
MatthiasChometon <matthiaschometon787@gmail.com>
2022-12-01 21:58:00 +0100

    feat: add GoogleNewsApiArticle and YahooFinanceNewsArticle

---
+++ src/articles/dto/google-news-api-article.ts
@@ -0,0 +1,33 @@
+import { CreateArticle } from '../models/create-article.input'
+
+export class GoogleNewsApiArticle {
+  symbol: string
+  source: {
+    id: string
+    name: string
+  }
+  author: string
+  title: string
+  description: string
+  url: string
+  urlToImage: string
+  publishedAt: Date
+  content: string
+
+  constructor(article: GoogleNewsApiArticle) {
+    Object.assign(this, article)
+  }
+
+  toCreateArticle(): CreateArticle {
+    const article: CreateArticle = {
+      title: this.title,
+      description: this.description,
+      source: this.source.name,
+      author: this.author,
+      publishedDate: new Date(this.publishedAt),
+      url: this.url,
+      symbol: null,
+    }
+    return new CreateArticle(article)
+  }
+}
