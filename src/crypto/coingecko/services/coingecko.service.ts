a87a4b0aa21d779f5fe3452a32d8e53ea84733c0
MatthiasChometon <matthiaschometon787@gmail.com>
2023-03-08 14:34:16 +0100

    fix: move apiUrl

--- src/crypto/coingecko/services/coingecko.service.ts
+++ src/crypto/coingecko/services/coingecko.service.ts
@@ -5,10 +5,8 @@ import { CryptoCurrencyMarket } from 'src/crypto/entities/cryptocurrency.entity'
 
 @Injectable()
 export class CoingeckoService {
-    constructor(
-        private readonly httpService: HttpService,
-        private readonly apiUrl: string = process.env.COINGECKO_API,
-    ) {}
+    constructor(private readonly httpService: HttpService,) { }
+    private readonly apiUrl: string = process.env.COINGECKO_API
 
     async getAllCoinsMarket(): Promise<CryptoCurrencyMarket[]> {
         const url = `${this.apiUrl}/coins/markets?vs_currency=EUR&order=market_cap_desc&per_page=100&page=1&sparkline=false`
