ba4eb70d5676a4398851027b7aadc05b3e28ee19
MatthiasChometon <matthiaschometon787@gmail.com>
2023-03-29 17:29:58 +0200

    feat: add investment import

--- src/investments/investment.module.ts
+++ src/investments/investment.module.ts
@@ -1,1 +1,19 @@
-import { Global, Module } from '@nestjs/common'import { TypeOrmModule } from '@nestjs/typeorm'import { InvestmentEntity } from './entities/investment.entity'import { InvestmentResolver } from './investment.resolver'import { InvestmentService } from './investment.service'@Global()@Module({    imports: [TypeOrmModule.forFeature([InvestmentEntity])],    providers: [InvestmentEntity, InvestmentResolver, InvestmentService],    exports: [TypeOrmModule],})export class InvestmentModule {}
+import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { InvestmentEntity } from './entities/investment.entity'
+import { InvestmentResolver } from './investment.resolver'
+import { InvestmentService } from './services/investment.service'
+import { ImportInvestmentService } from './services/import-investments.service'
+import { HttpModule } from '@nestjs/axios'
+
+@Module({
+    imports: [TypeOrmModule.forFeature([InvestmentEntity]), HttpModule],
+    providers: [
+        InvestmentEntity,
+        InvestmentResolver,
+        InvestmentService,
+        ImportInvestmentService,
+    ],
+    exports: [TypeOrmModule],
+})
+export class InvestmentModule {}
