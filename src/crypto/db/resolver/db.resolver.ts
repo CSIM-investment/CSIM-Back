d1d34dcb50f10746e847668d2934cd6681fc189a
Tim GIMENEZ <timgim2000@hotmail.fr>
2023-04-13 00:04:48 +0200

    feat: report calculs for generate investment

--- src/crypto/db/resolver/db.resolver.ts
+++ src/crypto/db/resolver/db.resolver.ts
@@ -5,6 +5,7 @@ import { CoingeckoService } from 'src/crypto/coingecko/services/coingecko.servic
 import { Cron } from '@nestjs/schedule'
 import { CryptoSearchInput } from '../dto/cryptoMarket-query'
 import { CryptoCurrencyMarketPaginatedResults } from '../dto/crypto-paginated-results'
+import { CryptoMarketInput } from '../dto/cryptoMarket-create.dto'
 
 @Resolver(CryptoCurrencyMarket)
 export class DbMutationResolver {
@@ -13,13 +14,41 @@ export class DbMutationResolver {
         private coinGeckoService: CoingeckoService,
     ) {}
 
-    // @Cron('10 * * * * *')
+    @Cron('10 * * * * *')
     @Mutation(() => String)
     async createCryptoMarket(): Promise<string> {
         this.coinGeckoService.getAllCoinsMarket().then((resp) => {
             resp.forEach((elmnt) => {
                 this.cryptoService.createCryptoCurrencyMarket(elmnt)
             })
+        })
+        this.cryptoService.createCryptoCurrencyMarket({
+            name: 'euro',
+            ath: 1,
+            atl: 1,
+            ath_change_percentage: 0,
+            ath_date: new Date(Date.now()),
+            atl_change_percentage: 0,
+            atl_date: new Date(Date.now()),
+            circulating_supply: 0,
+            current_price: 1,
+            fully_diluted_valuation: 0,
+            high_24h: 1,
+            market_cap: 0,
+            image: 'https://media.istockphoto.com/id/1173483815/fr/vectoriel/euro-sign-ic%C3%B4ne-de-glyphe-vectoriel.jpg?s=612x612&w=0&k=20&c=E_nl-VaEtDVxTLKIE0EKl3tO8F_Sr3eSBArVQjeNJzI=',
+            low_24h: 1,
+            market_cap_change_24h: 1,
+            last_updated: new Date(Date.now()),
+            market_cap_rank: 0,
+            market_cap_change_percentage_24h: 0,
+            max_supply: 0,
+            roi: '',
+            price_change_24h: 0,
+            symbol: 'eur',
+            id: 'euro',
+            price_change_percentage_24h: 0,
+            total_supply: 0,
+            total_volume: 0,
         })
         return 'datas has been created'
     }
--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,11 +1,212 @@
 import { Injectable } from '@nestjs/common'
 import { InvestmentReportDocument } from './documents/InvestmentReportDocument'
+import { InvestmentEntity } from '../investments/entities/investment.entity'
+import { InvestmentService } from '../investments/services/investment.service'
+import { ReportInvestmentsDataInterface } from './interfaces/ReportInvestmentsData.interface'
+import { GainOrLooseByCryptoInterfaceInterface } from './interfaces/GainByCryptoInterface.interface'
+import { CryptoCount } from './interfaces/CryptoCount'
 
 @Injectable()
 export class ReportService {
-    async generateInvestmentReports(options): Promise<Buffer> {
-        console.log(options)
+    constructor(private readonly investmentService: InvestmentService) {}
 
+    async isInvestmentSell(investment: InvestmentEntity): Promise<boolean> {
+        return investment.quoteCurrency.symbol == 'eur'
+    }
+
+    async isInvestmentBuy(investment: InvestmentEntity): Promise<boolean> {
+        return investment.baseCurrency.symbol == 'eur'
+    }
+
+    async getCryptosBuyAndSellSortedByCryptoSymbol(
+        investmentsList: InvestmentEntity[],
+    ): Promise<object> {
+        /**
+         * {
+         *     "sol": [{
+         *         investment: Investment,
+         *         amountSelled: 2
+         *     }]
+         * }
+         */
+        const cryptoBuy = {}
+        const cryptoSelled = {}
+
+        investmentsList.forEach((investment) => {
+            if (this.isInvestmentBuy(investment)) {
+                if (
+                    !cryptoBuy.hasOwnProperty(investment.quoteCurrency.symbol)
+                ) {
+                    cryptoBuy[investment.quoteCurrency.symbol] = []
+                }
+                cryptoBuy[investment.quoteCurrency.symbol].append(investment)
+            } else if (this.isInvestmentSell(investment)) {
+                if (!cryptoBuy.hasOwnProperty(investment.baseCurrency.symbol)) {
+                    cryptoBuy[investment.baseCurrency.symbol] = []
+                }
+                cryptoSelled[investment.baseCurrency.symbol].append(investment)
+            }
+        })
+        return { cryptoBuy, cryptoSelled }
+    }
+
+    private async countAllCryptoBuyOrSellBySymbol(cryptoBuyOfSymbol: InvestmentEntity[]) {
+        let numberOfCryptoOfSymbol = 0.0
+        await cryptoBuyOfSymbol.forEach((investmentEntity: InvestmentEntity) => {
+            numberOfCryptoOfSymbol += investmentEntity.valueQuoteCurrency * investmentEntity.quantity
+        })
+        return numberOfCryptoOfSymbol
+    }
+
+    private async calculCost(investmentEntity: InvestmentEntity): Promise<{ [key: string]: number }>{
+        let costToReturn = {}
+        costToReturn[investmentEntity.quoteCurrency.symbol] = investmentEntity.valueQuoteCurrency * investmentEntity.quantity
+        costToReturn[investmentEntity.baseCurrency.symbol] = investmentEntity.valueBaseCurrency * investmentEntity.quantity
+        return costToReturn
+    }
+
+    private async substractCryptoSelledUsingCryptoBuyBeforeDate(cryptoBuy: InvestmentEntity[], cryptoSelled: InvestmentEntity[], date){
+        cryptoSelled.forEach((selledInvestmentEntity: InvestmentEntity) => {
+            let costOfInvestmentSelled = this.calculCost(selledInvestmentEntity)
+            while (costOfInvestmentSelled[selledInvestmentEntity.quoteCurrency.symbol] !== 0 && cryptoBuy.length > 0) {
+                // soustraire le buyed crypto
+            }
+        })
+    }
+
+    private async separateInvestmentBuyAndSelledToGenerateReport(
+        investments: InvestmentEntity[],
+        cryptoBuy: {[key: string]: InvestmentEntity[]},
+        cryptoSelled: {[key: string]: InvestmentEntity[]},
+        beginDate: Date,
+        endDate: Date,
+    ): Promise<ReportInvestmentsDataInterface> {
+        const gainsByCrypto: GainOrLooseByCryptoInterfaceInterface[] = []
+        const loosesByCrypto: GainOrLooseByCryptoInterfaceInterface[] = []
+        const investmentsOfMonth: InvestmentEntity[] = []
+        const numberOfCryptoOnEndDate: CryptoCount[] = []
+        const numberOfCryptoOnStartDate: CryptoCount[] = []
+        let loosesOfMonth = 0,
+            gainsOfMonth = 0
+
+        for (const symbol in cryptoSelled) {
+            const cryptoSelledOfSymbol = cryptoSelled[symbol]
+            const cryptoBuyOfSymbol = cryptoBuy[symbol]
+
+            // Count all crypto
+            const countAllCryptoBuyed = this.countAllCryptoBuyOrSellBySymbol(cryptoBuyOfSymbol)
+            const countAllCryptoSelled = this.countAllCryptoBuyOrSellBySymbol(cryptoSelledOfSymbol)
+
+            // Substract cryptoSelledOfSymbol by cryptoBuyOfSymbol
+            this.substractCryptoSelledUsingCryptoBuyBeforeDate(cryptoBuyOfSymbol, cryptoSelledOfSymbol, beginDate)
+
+
+            numberOfCryptoOnStartDate =
+
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            if (crypto.baseCurrency.symbol === symbol) {
+                if (cryptoBuy.includes(crypto.baseCurrency.symbol)) {
+                    while (crypto.valueBaseCurrency * crypto.quantity !== 0) {
+                        cryptoBuyUsingThisSymbol.forEach(
+                            (cryptoBuyEntity: InvestmentEntity) => {
+                                if (crypto.dateOfInvestment < endDate)
+                                    if (
+                                        cryptoBuyEntity.valueBaseCurrency *
+                                        cryptoBuyEntity.quantity <
+                                        crypto.valueQuoteCurrency *
+                                        crypto.quantity
+                                    ) {
+                                    } else if (
+                                        cryptoBuyEntity.valueBaseCurrency *
+                                        cryptoBuyEntity.quantity ==
+                                        crypto.valueBaseCurrency *
+                                        crypto.quantity
+                                    ) {
+                                    } else {
+                                    }
+                            },
+                        )
+                    }
+                }
+            }
+        }
+
+        return {
+            investments: investmentsOfMonth,
+            numberOfCryptoOnEndDate: numberOfCryptoOnEndDate,
+            numberOfCryptoOnBeginDate: numberOfCryptoOnStartDate,
+            gainByCrypto: gainsByCrypto,
+            looseByCrypto: loosesByCrypto,
+            gainOfMonth: gainsOfMonth,
+            looseOfMonth: loosesOfMonth,
+            startDate: beginDate,
+            endDate: endDate,
+        }
+    }
+
+    async generateReportDataUsingInvestmentsList(
+        investmentsList: InvestmentEntity[],
+        start_date: Date,
+        end_date: Date,
+    ): Promise<ReportInvestmentsDataInterface> {
+        let gains = 0
+        let loose = 0
+        let investmentsBetweenDates = []
+        let cryptoOnBeginDate = []
+        let cryptoOnEndDate = []
+
+        // Get crypto
+
+        /**
+         * {
+         *     "sol": [{
+         *         investment: Investment,
+         *         amountSelled: 2
+         *     }]
+         * }
+         */
+
+        let { cryptoBuy, cryptoSelled } =
+            await this.getCryptosBuyAndSellSortedByCryptoSymbol(investmentsList)
+
+        this.separateInvestmentBuyAndSelledToGenerateReport(
+            cryptoBuy,
+            cryptoSelled,
+            start_date,
+            end_date,
+        )
+
+        return {
+            startDate: start_date,
+            endDate: end_date,
+            gainOfMonth: gains,
+            looseOfMonth: loose,
+            investments: investmentsBetweenDates,
+            numberOfCryptoOnBeginDate: cryptoOnBeginDate,
+            numberOfCryptoOnEndDate: cryptoOnEndDate,
+            gainByCrypto: [],
+            looseByCrypto: [],
+        }
+    }
+
+    async generateInvestmentReports(options, user): Promise<Buffer> {
+        console.log(options.toDate)
+        let investmentService =
+            await this.investmentService.getInvestementsByUserIdAndEndDateOfInvestment(
+                user ? user.id : null,
+                new Date(options.toDate),
+            )
+
+        await this.generateReportDataUsingInvestmentsList(
+            investmentService,
+            options.fromDate,
+            options.toDate,
+        )
+
+        console.log(investmentService)
+
+        const invest1 = new InvestmentEntity()
         const investmentReport = new InvestmentReportDocument({
             investments: ['Investment 1', 'Investment 2', 'Investment 3'],
             gains: [
@@ -19,5 +220,18 @@ export class ReportService {
         })
         investmentReport.generate_investment_report_using_investments()
         return await investmentReport.to_pdf()
+    }
+
+    async soldUser(userId: number): Promise<number> {
+        let sold = 0
+        const investments = []
+        investments.forEach((item) => {
+            const price =
+                item.quantity *
+                item.valueBaseCurrency *
+                item.baseCurrency.current_price
+            sold += price
+        })
+        return Number(sold)
     }
 }
