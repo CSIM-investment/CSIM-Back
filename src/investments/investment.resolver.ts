c31074e06ce830a1c62aaab2076d937dedb1d59a
MatthiasChometon <matthiaschometon787@gmail.com>
2023-04-19 12:01:22 +0200

    feat: remove dashboard

--- src/investments/investment.resolver.ts
+++ src/investments/investment.resolver.ts
@@ -6,7 +6,6 @@ import {
     Parent,
     ResolveField,
     Resolver,
-    Query,
 } from '@nestjs/graphql'
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard'
 import { CreateInvestmentInput } from './dto/createInvestments.input'
@@ -15,7 +14,6 @@ import { InvestmentService } from './investment.service'
 import { User } from 'src/user/methods/user.methods'
 import { UserService } from 'src/user/user.service'
 import { CoingeckoService } from 'src/crypto/coingecko/services/coingecko.service'
-import { CryptoCurrencyMarket } from 'src/crypto/entities/cryptocurrency.entity'
 
 @Resolver(() => InvestmentEntity)
 export class InvestmentResolver {
@@ -42,41 +40,5 @@ export class InvestmentResolver {
     @UseGuards(JwtAuthGuard)
     investments(@Parent() { id }: User): Promise<InvestmentEntity[]> {
         return this.investmentService.getInvestementsByUserId(id)
-    }
-
-    @Query(() => [Number])
-    // eslint-disable-next-line @typescript-eslint/ban-types
-    async dashboard(@Args('id') id: number): Promise<{}> {
-        const newSold = await this.sold(id)
-        const lastSold = await this.lastSold(id)
-        const fourthCryptos = await this.fourthCryptos()
-
-        const diffSold = newSold / lastSold
-
-        const spent = 2000.22
-        console.log(fourthCryptos)
-        const datas = {
-            newSold: newSold,
-            lastSold: lastSold,
-            diffSold: diffSold,
-            spent: spent,
-            fourthCryptos: fourthCryptos,
-        }
-        return datas
-    }
-
-    @Query(() => Number)
-    async sold(@Args('id') id: number): Promise<number> {
-        return await this.investmentService.soldUser(id)
-    }
-
-    @Query(() => Number)
-    async lastSold(@Args('id') id: number): Promise<number> {
-        return await this.userService.getUserSold(id)
-    }
-
-    @Query(() => Number)
-    async fourthCryptos(): Promise<CryptoCurrencyMarket[]> {
-        return await this.coingeckoService.getFourthCryptos()
     }
 }
