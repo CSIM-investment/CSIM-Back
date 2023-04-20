09b2a2e2b0166824f4a09160d1286fed047cfe4b
MatthiasChometon <matthiaschometon787@gmail.com>
2023-04-20 09:57:58 +0200

    feat: add lastInvestments and topInvestments

--- src/investments/sold.resolver.ts
+++ src/investments/sold.resolver.ts
@@ -2,7 +2,6 @@ import { Parent, ResolveField, Resolver } from '@nestjs/graphql'
 import { User } from 'src/user/methods/user.methods'
 import { UserSold } from './dto/user-sold.output'
 import { UserService } from 'src/user/user.service'
-import { CoingeckoService } from 'src/crypto/coingecko/services/coingecko.service'
 import { InvestmentService } from './services/investment.service'
 
 @Resolver(() => User)
@@ -10,25 +9,27 @@ export class SoldResolver {
     constructor(
         private readonly investmentService: InvestmentService,
         private readonly userService: UserService,
-        private readonly coingeckoService: CoingeckoService,
     ) {}
 
     @ResolveField(() => UserSold)
     async sold(@Parent() { id, sold: currentSold }: User): Promise<UserSold> {
-        const newSold = await this.investmentService.soldUser(id)
-        const lastSold = await this.userService.getUserSold(id)
-        const fourthCryptos = await this.coingeckoService.getFourthCryptos()
+        const [newSold, lastSold, lastInvestments, topInvestments] =
+            await Promise.all([
+                this.investmentService.soldUser(id),
+                this.userService.getUserSold(id),
+                this.investmentService.getMostRecentInvestments(id),
+                this.investmentService.getTopInvestments(id),
+            ])
         const soldRatio = lastSold === 0 ? 0 : newSold / lastSold
 
         const sold: UserSold = {
             newSold,
             lastSold,
-            fourthCryptos,
+            lastInvestments,
             soldRatio,
             currentSold,
+            topInvestments,
         }
-
-        console.log({ sold })
 
         return sold
     }
