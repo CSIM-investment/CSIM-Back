618a158ad6cf8dbbcbb12333cd46da60429c947f
MatthiasChometon <matthiaschometon787@gmail.com>
2023-05-09 19:48:14 +0200

    feat: can choose hours for articles update

--- src/articles/article.resolver.ts
+++ src/articles/article.resolver.ts
@@ -1,1 +1,30 @@
-import { Args, Mutation, Query, Resolver } from '@nestjs/graphql'import { ArticleService } from './services/article.service'import { Article } from './methods/article.methods'import { Cron } from '@nestjs/schedule'import { ArticlesInput } from './dto/inputs/articles.input'@Resolver()export class ArticleResolver {    constructor(private readonly articleService: ArticleService) {}    @Mutation(() => String)    // @Cron('0 0 4 * * *')    updateArticles(): Promise<string> {        return this.articleService.update()    }    // @Cron('0 0 0 1 * *')    deleteArticlesFromYesterday(): Promise<string> {        return this.articleService.deleteFromYesterday()    }    @Query(() => [Article])    articles(@Args('options') options: ArticlesInput): Promise<Article[]> {        return this.articleService.search(options)    }}
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql'
+import { ArticleService } from './services/article.service'
+import { Article } from './methods/article.methods'
+import { Cron } from '@nestjs/schedule'
+import { ArticlesInput } from './dto/inputs/articles.input'
+import { Int } from '@nestjs/graphql'
+
+@Resolver()
+export class ArticleResolver {
+    constructor(private readonly articleService: ArticleService) {}
+
+    @Mutation(() => String)
+    // @Cron('0 0 4 * * *')
+    updateArticles(
+        @Args({ name: 'hours', type: () => Int, defaultValue: 4 })
+        hours: number,
+    ): Promise<string> {
+        return this.articleService.update(hours)
+    }
+
+    // @Cron('0 0 0 1 * *')
+    deleteArticlesFromYesterday(): Promise<string> {
+        return this.articleService.deleteFromYesterday()
+    }
+
+    @Query(() => [Article])
+    articles(@Args('options') options: ArticlesInput): Promise<Article[]> {
+        return this.articleService.search(options)
+    }
+}
