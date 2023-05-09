2b075772724ed571c7569d0b577d59cc35f2cb79
MatthiasChometon <matthiaschometon787@gmail.com>
2023-05-09 19:47:36 +0200

    fix: send 0 if not insert

--- src/articles/repositories/article.repository.ts
+++ src/articles/repositories/article.repository.ts
@@ -1,1 +1,20 @@
-import { Injectable } from '@nestjs/common'import { Repository, DataSource } from 'typeorm'import { Article } from '../methods/article.methods'import { CreateArticle } from '../models/create-article.input'@Injectable()export class ArticleRepository extends Repository<Article> {    constructor(private dataSource: DataSource) {        super(Article, dataSource.createEntityManager())    }    async saveOrIgnore(articles: CreateArticle[]): Promise<number> {        const queryResult = await this.createQueryBuilder()            .insert()            .values(articles)            .orIgnore()            .execute()        return queryResult.raw.length    }}
+import { Injectable } from '@nestjs/common'
+import { Repository, DataSource } from 'typeorm'
+import { Article } from '../methods/article.methods'
+import { CreateArticle } from '../models/create-article.input'
+
+@Injectable()
+export class ArticleRepository extends Repository<Article> {
+    constructor(private dataSource: DataSource) {
+        super(Article, dataSource.createEntityManager())
+    }
+
+    async saveOrIgnore(articles: CreateArticle[]): Promise<number> {
+        const queryResult = await this.createQueryBuilder()
+            .insert()
+            .values(articles)
+            .orIgnore()
+            .execute()
+        return queryResult.raw?.length ?? 0
+    }
+}
